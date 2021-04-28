7b3d67b3dd4e36dd2c7f71d6709bc9025f13d2c4
Mike <mullenkamp1gmail.com>
2021-04-28 13:19:23 +1200

    update

--- tethysts/tests/utest_tethysts.py
+++ tethysts/tests/utest_tethysts.py
@@ -19,7 +19,7 @@ remotes_list = param['remotes']
 ### Testing
 
 # remote = {'bucket': 'fire-emergency-nz', 'connection_config': 'https://b2.tethys-ts.xyz'}
-remote = remotes_list[0]
+remote = remotes_list[-1]
 # remote['connection_config'] = 'https://b2.tethys-ts.xyz'
 #
 dataset_id = '4edc94c19bf074027bc7c099'
@@ -27,14 +27,14 @@ dataset_id = 'dddb02cd5cb7ae191311ab19'
 station_id = 'fedeb59e6c7f47597a7d47c7'
 # station_id = 'fe9a63fae6f7fe58474bb3c0'
 station_id = '6b75a7fb1453ef94148bda19'
-# station_ids = [station_id, 'fe9a63fae6f7fe58474bb3c0']
-# dataset_id='73ab8b02fc65686636eb6d0b'
-# station_id='489de57745076fb4ff2bd91b'
+# station_ids = [station_id, '5d06c5a8065a26b51c19b241']
+# dataset_id='361ce2acd56b13da82390a69'
+# station_id='00128a218015a069cb94d360'
 # dataset_id='ad3156ce8245f725a5a0cda8'
 # station_id='6b5dc8c9ec394b2c4167f6b8'
 #
 #
-# self = Tethys([remote])
+self = Tethys([remote])
 self = Tethys(remotes_list)
 #
 stn_list1 = self.get_stations(dataset_id)
@@ -46,7 +46,7 @@ data1 = self.get_results(dataset_id, station_id, remove_height=True, output='Dat
 # data1 = self.get_results(dataset_id, station_id, output='Dict')
 # data1 = self.get_results(dataset_id, station_id, from_date='2012-01-02 00:00', output='Dataset')
 
-# data2 = self.get_bulk_results(dataset_id, station_ids, remove_height=True, output='DataArray')
+# data2 = self.get_bulk_results(dataset_id, station_ids, output='Dataset')
 
 # dataset_id = 'f4cfb5a362707785dd39ff85'
 # station_id = 'ff4213c61878e098e07df513'
